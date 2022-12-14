7b043f961b389e196413c291592f85d2c47402b7
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-14 12:19:27 +0100

    Create test for IdService

---
+++ backend/src/test/java/service/IdServiceTest.java
@@ -0,0 +1,18 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class IdServiceTest {
+
+    IdService idService = new IdService();
+
+    @Test
+    void when_id_is_generated_than_should_not_return_empty_string() {
+        // When
+        String actual = idService.generateUuid();
+        // Then
+        assertNotNull(actual);
+    }
+}
