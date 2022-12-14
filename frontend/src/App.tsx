d811c90fb68aa8a575cc97260309e57bd191cdff
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-14 15:00:15 +0100

    Update app to show companies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import CompanyGallery from "./components/CompanyGallery";
+import {Company} from "./components/model/Company";
+import {getAllCompanies} from "./components/api/api-service";
 
 function App() {
+
+  const [companies, setCompanies] = useState<Company[]>([])
+
+  useEffect(() => {
+    getCompanies()
+  }, [])
+
+  function getCompanies(){
+    getAllCompanies
+        .then(setCompanies)
+        .catch(e => console.error(e))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CompanyGallery companies={companies} />
   );
 }
 
