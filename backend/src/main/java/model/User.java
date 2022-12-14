c61ba40913d494c77850e3ab45e22eb915ef7094
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-14 12:57:41 +0100

    Create User model

---
+++ backend/src/main/java/model/User.java
@@ -0,0 +1,19 @@
+package model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document("users")
+public record User(
+        @Id
+        String id,
+        String firstname,
+        String lastname,
+        String username,
+        String password,
+        String email,
+        List<Company> companies
+) {
+}
