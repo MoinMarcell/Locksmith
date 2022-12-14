6b94ce9a73addb1cd735cacc19dbed0f7548b545
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-14 12:57:49 +0100

    Create Product model

---
+++ backend/src/main/java/model/Product.java
@@ -0,0 +1,16 @@
+package model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Document("products")
+public record Product(
+        @Id
+        String id,
+        String name,
+        String description,
+        BigDecimal price
+) {
+}
