59d7a2bd431ff49555be49ac5b030ff510dd07c3
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-14 12:48:40 +0100

    Create CompanyController

---
+++ backend/src/main/java/controller/CompanyController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import model.Company;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.CompanyService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("companies")
+public class CompanyController {
+
+    CompanyService companyService;
+
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping
+    public List<Company> getAllCompanies(){
+        return this.companyService.getAllCompanies();
+    }
+
+}
