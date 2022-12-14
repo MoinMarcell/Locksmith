30e324e6a0a90f3720297b385825baf1a0656f6a
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-14 15:06:14 +0100

    Update app with hook for companies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,9 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
 import CompanyGallery from "./components/CompanyGallery";
-import {Company} from "./components/model/Company";
-import {getAllCompanies} from "./components/api/api-service";
+import useCompanies from "./components/hooks/useCompanies";
 
 function App() {
 
-  const [companies, setCompanies] = useState<Company[]>([])
-
-  useEffect(() => {
-    getCompanies()
-  }, [])
-
-  function getCompanies(){
-    getAllCompanies
-        .then(setCompanies)
-        .catch(e => console.error(e))
-  }
+  const {companies} = useCompanies()
 
   return (
     <CompanyGallery companies={companies} />
