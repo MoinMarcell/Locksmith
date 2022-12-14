be51db5722aa52cda8e9be232726d4c451c0f09d
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-14 16:16:27 +0100

    Update idservice

--- backend/src/test/java/service/CompanyServiceTest.java
+++ backend/src/test/java/service/CompanyServiceTest.java
@@ -2,6 +2,7 @@ package service;
 
 import com.github.moinmarcell.backend.model.Company;
 import com.github.moinmarcell.backend.service.CompanyService;
+import com.github.moinmarcell.backend.service.IdService;
 import org.junit.jupiter.api.Test;
 import com.github.moinmarcell.backend.repository.CompanyRepository;
 
@@ -14,8 +15,9 @@ import static org.mockito.Mockito.*;
 class CompanyServiceTest {
 
     CompanyRepository companyRepository = mock(CompanyRepository.class);
+    IdService idServiceTest = mock(IdService.class);
 
-    CompanyService companyService = new CompanyService(companyRepository);
+    CompanyService companyService = new CompanyService(companyRepository, idServiceTest);
 
     @Test
     void getAllCompanies() {
